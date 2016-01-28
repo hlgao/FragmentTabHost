adb2fdff85e5f279eafc2b0b3d7d7628de659e1f
gaolonglong0 <gaolonglong0@gmail.com>
2016-01-28 18:12:50 +0800

    first commit

---
+++ app/src/main/java/com/app/gaolonglong/fragmenttabhost/MessageFragment.java
@@ -0,0 +1,31 @@
+package com.app.gaolonglong.fragmenttabhost;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by donglinghao on 2016-01-28.
+ */
+public class MessageFragment extends Fragment {
+
+    private View mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (mRootView == null){
+            Log.e("666","MessageFragment");
+            mRootView = inflater.inflate(R.layout.message_fragment,container,false);
+        }
+        ViewGroup parent = (ViewGroup) mRootView.getParent();
+        if (parent != null){
+            parent.removeView(mRootView);
+        }
+        return mRootView;
+    }
+}
---
+++ app/src/main/java/com/app/gaolonglong/fragmenttabhost/ReportFragment.java
@@ -0,0 +1,31 @@
+package com.app.gaolonglong.fragmenttabhost;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by donglinghao on 2016-01-28.
+ */
+public class ReportFragment extends Fragment {
+
+    private View mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (mRootView == null){
+            Log.e("666","ReportFragment");
+            mRootView = inflater.inflate(R.layout.report_fragment,container,false);
+        }
+        ViewGroup parent = (ViewGroup) mRootView.getParent();
+        if (parent != null){
+            parent.removeView(mRootView);
+        }
+        return mRootView;
+    }
+}
